c26c4ca211815815fbbf8610a8d7424dda72428d
praveen <149228650+Praveenkumarreddy12@users.noreply.github.com>
2025-07-22 17:19:28 +0530

    Update admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Customer, Books_available, Books_ordered
-# Register your models here.
 
+
+# Register your models here.
 admin.site.register(Customer)
 admin.site.register(Books_ordered)
 admin.site.register(Books_available)
